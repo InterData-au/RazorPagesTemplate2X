868d4c090f69e36852488b0f51231dad1df68256
dush-a <dushabe@gmail.com>
2022-05-25 10:51:54 +1000

    New Files
    Added new code files for the completed template.

---
+++ RazorPages21/Pages/About.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using RazorPages21.Extras;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RazorPages21.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHostingEnvironment _env;
+        public string OSDescription;
+        public string ProcessArchitecture;
+        public string VersionText;
+        public string DotNetCoreVersion;
+        public string ThisEnvironment { get; set; }
+        public string MyApplicationName { get; set; }
+        public string SolutionName { get; set; }
+        public string TargetFramework { get; set; }
+        public string Message { get; set; }
+
+        public AboutModel(ILogger<IndexModel> logger, IHostingEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+
+        public void OnGet()
+        {
+            SolutionName = DevFileUtilities.GetSolutionFileName();
+            ThisEnvironment = _env.EnvironmentName;
+            MyApplicationName = _env.ApplicationName;
+
+            var asembli = Assembly.GetExecutingAssembly();
+            var atrib = asembli.CustomAttributes.FirstOrDefault(a => a.AttributeType == typeof(TargetFrameworkAttribute));
+            TargetFramework = atrib?.ConstructorArguments[0].Value.ToString();
+
+            OSDescription = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            ProcessArchitecture = System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture.ToString();
+
+            VersionText = "Version: " + DevFileUtilities.GetNetCoreVersion() + " installed in: " + @System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory();
+
+            DotNetCoreVersion = DevFileUtilities.GetNetCoreVersion();
+
+
+            _logger.LogInformation("Completed Index.OnGet, {ThisEnvironment}!", ThisEnvironment);
+
+        }
+
+    }
+
+}
---
+++ RazorPages22/Pages/About.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Hosting;
+using RazorPages22.Extras;
+using System.Reflection;
+using System.Runtime.Versioning;
+
+namespace RazorPages22.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly IHostingEnvironment _env;
+        public string OSDescription;
+        public string ProcessArchitecture;
+        public string VersionText;
+        public string DotNetCoreVersion;
+        public string NetFrameworkVer;
+
+        public string ThisEnvironment { get; set; }
+      //  public string CurrentRunTimeVersion { get; set; }
+        public string MyApplicationName { get; set; }
+        public string SolutionName { get; set; }
+        public string TargetFramework { get; set; }
+
+
+
+        public AboutModel(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+        public void OnGet()
+        {
+            SolutionName = DevFileUtilities.GetSolutionFileName();
+            ThisEnvironment = _env.EnvironmentName;
+            MyApplicationName = _env.ApplicationName;
+
+            var atrib = Assembly.GetExecutingAssembly().CustomAttributes.FirstOrDefault(a => a.AttributeType == typeof(TargetFrameworkAttribute));
+            TargetFramework = atrib?.ConstructorArguments[0].Value.ToString();
+                        
+            OSDescription = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            ProcessArchitecture = System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture.ToString();
+
+            VersionText = "Version: " + DevFileUtilities.GetNetCoreVersion() + " installed in: " + @System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory();
+                      
+            DotNetCoreVersion = DevFileUtilities.GetNetCoreVersion();
+            NetFrameworkVer = SystemHelpers.GetInstalledDotNetFrameworkVersion();
+        }
+
+    }
+}
